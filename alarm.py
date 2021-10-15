654a2bfb3b5d47c6f2f6891d8bc838096abe9a7e
Robert Rüdiger <ruerob@t-online.de>
2021-10-15 13:50:09 +0200

    - fixed pomodoro bug that occured if system is hybernated

--- alarm.py
+++ alarm.py
@@ -78,13 +78,14 @@ def print_time_until_alarm(seconds: int, /):
 	print("Alarm starts in {} hour(s), {} minute(s), and {} second(s)".format(needed_hour, needed_min, needed_sec))
 
 
-def calc_secs_to_time(hour: int, minutes: int, /) -> int:
+def calc_secs_to_time(hour: int, minutes: int, seconds: int = 0, /) -> int:
 	""" Calculates the amount of seconds until the alarm is supposed to ring
 
 	e.g: calc_secs_to_tim(14, 9)
 
 	[hour: int] The clocks hour value at the alarm ring time.
 	[minutes: int] The clocks minute value at the alarm ring time.
+	[seconds: int = 0] The clocks second value at the alarm ring time.
 
 	[returns: int] Seconds remaining until the alarm rings.
 	"""
@@ -120,29 +121,36 @@ def calc_secs_to_time(hour: int, minutes: int, /) -> int:
 		needed_hour = 24
 
 	# Return the seconds remaining until the alarm rings.
-	return needed_min*60 + needed_hour*3600 - now.tm_sec
+	return needed_min*60 + needed_hour*3600 - now.tm_sec + seconds
 
 
-def start_alarm_clock(alarm_hour: int, alarm_min: int, /):
+def start_alarm_clock(alarm_hour: int, alarm_min: int, alarm_sec: int = 0, /):
 	""" Starts an alarm that rings at the specified time.
 
 	e.g.: startAlarmClock(14,9)
 
-	[alarm_hour: int] the hour value of the time.
-	[alarm_min: int] the minute value of the time.
+	[alarm_hour: int] the hour value of the alarm time.
+	[alarm_min: int] the minute value of the alarm time.
+	[alarm_sec: int = 0] the seconds of the alarm time.
 	"""
 	
 	# Here we calc how many seconds we have to wait.
-	wait_seconds = calc_secs_to_time(alarm_hour, alarm_min)
+	wait_seconds = calc_secs_to_time(alarm_hour, alarm_min, alarm_sec)
 
 	# Sleeping time!
+	# While there are still seconds to wait
 	while wait_seconds > 0:
-		wait_seconds = calc_secs_to_time(alarm_hour, alarm_min)
+		# calc how many seconds are still on the clock
+		wait_seconds = calc_secs_to_time(alarm_hour, alarm_min, alarm_sec)
+		# Tell the user about the waiting time
 		print_time_until_alarm(wait_seconds)
-		wait_seconds = wait_seconds - 60
-		if wait_seconds < 0:
-			time.sleep(60+wait_seconds)
+
+		# Check if there are less than 60 seconds left.
+		if wait_seconds < 60:
+			# Sleep for the last seconds.
+			time.sleep(wait_seconds)
 		else:
+			# Sleep for 60 seconds and check again
 			time.sleep(60)
 	
 	# And time to wake up!!
@@ -156,29 +164,43 @@ def start_pomodoro(minutes: int, /):
 	[minutes: int] In how many minutes the alarm should start.
 	"""
 
-	# Tell the user that the alarm will ring in the passed amount of minutes.
-	print("Alarm rings in {} minute(s)".format(minutes))
-	
-	# Wait for the seconds.
-	time.sleep(minutes*60)
-	
-	# Do the alarm call.
-	print("Wake up!!!")
-	_ring(5)
+	# Which time is it now
+	now = time.localtime()
+
+	# We take the currents time seconds value as alarm ring time
+	alarm_sec = now.tm_sec
+
+	# We take the current time minutes value and add the minutes the timer should be set to.
+	alarm_min = now.tm_min + minutes
+
+	# We take the current times hour.
+	alarm_hour = now.tm_hour
+
+	# For every 60 minutes, we add an hour to the alarm time.
+	while alarm_min >= 60:
+		alarm_min = alarm_min - 60
+		alarm_hour = alarm_hour + 1
+
+	# If the timer should ring the next day, we need to reduce the alarm hour by 24.
+	if alarm_hour > 23:
+		alarm_hour = alarm_hour - 24
+
+	# We set the alarm clock to the calculated time.
+	start_alarm_clock(alarm_hour, alarm_min, alarm_sec)
 
 
 def _print_argument_error():
 	""" Prints the help text"""
 
+	print("")
 	print("Small alarm function for your console.")
 	print("Usable with one or two numeric arguments")
-	print()
-	print("Sox must be installed with 'sudo apt-get install sox' to get an alarm sound.")
-	print()
+	print("")
 	print("One argument:")
 	print("If there is only one argument it is interpreted as minutes. The alarm will act")
-	print("as a pomodoro that rings after the set minutes.")
-	print()
+	print("as a pomodoro that rings after the set minutes. The maximum amount of minutes")
+	print("is 1439.")
+	print("")
 	print("Two arguments:")
 	print("With two arguments, you will set a alarm clock for a specified time.")
 	print("If you set 14 09 as arguments, the alarm will start at 14:09.")
@@ -190,8 +212,24 @@ def _print_argument_error():
 # If the module is run as a script.
 if __name__ == "__main__":
 
+	# If the user entered one numeric parameter.
+	if len(sys.argv) == 2 and sys.argv[1].isnumeric():
+
+		# Load the parameter as minutes.
+		arg_minutes = (int(sys.argv[1]))
+
+		# Check if the pomodoro is set to a reasonable time.
+		if 0 <= arg_minutes < 1440:
+
+			# Start the pomodoro.
+			start_pomodoro(arg_minutes)
+
+		else:
+			# Else we tell the user how he can use this tool.
+			_print_argument_error()
+
 	# If the user entered two numeric parameter.
-	if len(sys.argv) == 3 and sys.argv[1].isnumeric() and sys.argv[2].isnumeric():
+	elif len(sys.argv) == 3 and sys.argv[1].isnumeric() and sys.argv[2].isnumeric():
 	
 		# The first parameter is the hour value of the set time.
 		arg_hour = int(sys.argv[1])
@@ -204,22 +242,6 @@ if __name__ == "__main__":
 			start_alarm_clock(arg_hour, arg_minute)
 		else:
 			# Else we let the user know how to use this tool.
-			_print_argument_error()
-	
-	# If the user entered one numeric parameter.
-	elif len(sys.argv) == 2 and sys.argv[1].isnumeric():
-	
-		# Load the parameter as minutes.
-		arg_minutes = (int(sys.argv[1]))
-		
-		# Check if the pomodoro is set to a reasonable time.
-		if arg_minutes >= 0:
-		
-			# Start the pomodoro.
-			start_pomodoro(arg_minutes)
-	
-		else:
-			# Else we tell the user how he can use this tool.
 			_print_argument_error()
 			
 	# Else there where the wrong number of arguments or they had the wrong type.
